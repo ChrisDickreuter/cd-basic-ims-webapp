30390ecb226fbf662c2d771dc1e41d9f5e337571
Christian Dickreuter <christian.dickreuter@gmail.com>
2024-05-05 21:07:45 +0200

    Added temporary items for dev

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 
-const items = ref<string[]>([])
+const items = ref<Object[]>([])
 
 onMounted(() => {
-  items.value = ['item1', 'item2', 'item3']
+  items.value = [
+    { id: 1, title: 'item1', description: 'description1' },
+    { id: 2, title: 'item2', description: 'description2' },
+    { id: 3, title: 'item3', description: 'description3' }
+  ]
 })
 </script>
 
