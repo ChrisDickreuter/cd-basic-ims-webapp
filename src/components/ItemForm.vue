5b6fb0471467b3bd6507920f4db29ef40f39786a
Christian Dickreuter <christian.dickreuter@sv-technologie.de>
2024-05-22 16:17:24 +0200

    Add item form

---
+++ src/components/ItemForm.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const emit = defineEmits(['add-item'])
+
+const title = ref('')
+const description = ref('')
+
+const onSubmit = () => {
+    const item = { title: title.value, description: description.value }
+    emit('add-item', item)
+    title.value = ''
+    description.value = ''
+}
+</script>
+
+<template>
+    <form @submit.prevent="onSubmit">
+        <label for="title">Title</label>
+        <input type="text" v-model="title" name="title" required>
+        <label for="description">Description</label>
+        <textarea v-model="description" name="description" required></textarea>
+        <button type="submit">Add</button>
+    </form>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import ItemList from '@/components/ItemList.vue'
+import ItemForm from "@/components/ItemForm.vue";
 
 const items = ref<Object[]>([])
 
@@ -11,11 +12,20 @@ onMounted(() => {
     { id: 3, title: 'item3', description: 'description3' }
   ]
 })
+
+const addItem = (item: { title: string, description: string }) => {
+  items.value.push({
+    id: items.value.length + 1,
+    title: item.title,
+    description: item.description
+  })
+}
 </script>
 
 <template>
   <main>
     <h1>Home</h1>
     <ItemList :items="items" />
+    <ItemForm @add-item="addItem" />
   </main>
 </template>
